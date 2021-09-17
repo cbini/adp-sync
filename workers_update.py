8b49d79799c72e0f37e39c1737a44ca4d03a227a
Charlie Bini <cbini87@gmail.com>
2021-09-16 23:09:30 -0500

    refactor auth and api calls

--- workers_update.py
+++ workers_update.py
@@ -4,64 +4,27 @@ import os
 import pathlib
 import traceback
 
-import requests
 from dotenv import load_dotenv
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+
+import adp
 
 load_dotenv()
 
-CERT_FILENAME = os.getenv("CERT_FILENAME")
-KEY_FILENAME = os.getenv("KEY_FILENAME")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CERT_FILEPATH = os.getenv("CERT_FILEPATH")
+KEY_FILEPATH = os.getenv("KEY_FILEPATH")
 ADP_IMPORT_FILE = os.getenv("ADP_IMPORT_FILE")
 ADP_EXPORT_FILE = os.getenv("ADP_EXPORT_FILE")
 
-SECURITY_TOKEN_SERVICE = "accounts.adp.com"
-SERVICE_ROOT = "api.adp.com"
 WORKER_ENDPOINT = "/events/hr/v1/worker"
-TOKEN_URL = f"https://{SECURITY_TOKEN_SERVICE}/auth/oauth/v2/token"
 
 PROJECT_PATH = pathlib.Path(__file__).absolute().parent
-CERT_PATH = PROJECT_PATH / "certs" / CERT_FILENAME
-KEY_PATH = PROJECT_PATH / "certs" / KEY_FILENAME
-CERT_TUPLE = (CERT_PATH, KEY_PATH)
-
-
-def post_worker_change(session, path, payload):
-    change_url = f"https://{SERVICE_ROOT}{WORKER_ENDPOINT}.{path}.change"
-
-    try:
-        r_change = session.post(change_url, json=payload)
-        r_change.raise_for_status()
-    except:
-        resource_messages = (
-            r_change.json().get("confirmMessage").get("resourceMessages")
-        )
-        process_messages = [m.get("processMessages") for m in resource_messages]
-
-        formatted_message = f"\t{change_url}\n\t{payload}\n\n"
-        for m in process_messages:
-            print(f"message: {m}")
-            formatted_message += (
-                f"\t{r_change.status_code} - {r_change.reason}: "
-                f"{m.get('userMessage').get('messageTxt')}"
-            )
-        print(formatted_message)
 
 
 def main():
     print("Authenticating with ADP...")
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    client = BackendApplicationClient(client_id=CLIENT_ID)
-    session = OAuth2Session(client=client)
-    session.cert = CERT_TUPLE
-    token_dict = session.fetch_token(token_url=TOKEN_URL, auth=auth)
-
-    # attach auth to session
-    access_token = token_dict.get("access_token")
-    session.headers["Authorization"] = f"Bearer {access_token}"
+    adp_client = adp.authorize(CLIENT_ID, CLIENT_SECRET, CERT_FILEPATH, KEY_FILEPATH)
     print("\tSUCCESS!")
 
     print("Loading db import data...")
@@ -131,7 +94,7 @@ def main():
         )
 
         if record_match:
-            ## update work email if new
+            # update work email if new
             if i["mail"] != record_match["work_email"]:
                 print(
                     f"{i['employee_number']}"
@@ -143,10 +106,11 @@ def main():
                     "worker"
                 ] = {"businessCommunication": {"email": {"emailUri": i["mail"]}}}
 
-                post_worker_change(
-                    session=session,
+                adp.post(
+                    session=adp_client,
                     path="business-communication.email",
                     payload=work_email_change_payload,
+                    action="change",
                 )
 
             # update employee number if missing
@@ -171,10 +135,11 @@ def main():
                     "stringField": {"stringValue": i["employee_number"]}
                 }
 
-                post_worker_change(
-                    session=session,
+                adp.post(
+                    session=adp_client,
                     path="custom-field.string",
                     payload=employee_number_change_payload,
+                    action="change",
                 )
 
     print("SUCCESS!")
